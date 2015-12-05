fcddc9f96c84b321c1d1e2834e7fcc481e8d14ea
neo1218 <neo1218@yeah.net>
2015-12-06 04:28:28 +0800

    add :class AuthUser:

---
+++ rest/auth/__init__.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+
+from .authenticate import AuthUser
---
+++ rest/auth/authenticate.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+class AuthUser(object):
+    from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+    from flask import current_app
+
+    def generate_auth_token(self, expiration):
+        """generate a token"""
+        s = Serializer(
+            current_app.config['SECRET_KEY'],
+            expiration
+        )
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        """verify the user with token"""
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get_or_404(data['id'])
